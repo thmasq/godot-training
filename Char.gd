01fce5b1d7ed155b7ce39181c479b340623557f1
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-11-15 19:37:26 -0300

    GLES3 -> GLES2 and improves character mobility

--- Char.gd
+++ Char.gd
@@ -1,21 +1,40 @@
 extends KinematicBody2D
-
 var velocity = Vector2.ZERO
+var fast_fell = false
+export(int) var JUMP_FORCE = -300
+export(int) var JUMP_RELEASE_FORCE = -60
+export(int) var MAX_SPEED = 100
+export(int) var ACCELERATION = 10
+export(int) var FRICTION = 30
+export(int) var ADDITIONAL_FALL_GRAVITY = 40
+export(int) var GRAVITY = 20
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	apply_gravity()
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+	if input.x == 0:
+		apply_friction()
 	else:
-		velocity.x = 0
-	
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -300
-	velocity = move_and_slide(velocity)
+		apply_acceleration(input.x)
+	if is_on_floor():
+		fast_fell = false
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_FORCE
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
+		if velocity.y > 10 and not fast_fell:
+			velocity.y += ADDITIONAL_FALL_GRAVITY
+		fast_fell = true
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func apply_gravity():
-		velocity.y += 20
+		velocity.y += GRAVITY
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, FRICTION )
+	
+func apply_acceleration(ammount):
+	velocity.x = move_toward(velocity.x, MAX_SPEED*ammount, ACCELERATION)
+	
